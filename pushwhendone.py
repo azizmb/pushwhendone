ef0688319de0cb77e9b0c5dbb178dea96e7d780d
Aziz M. Bookwala <me@azizmb.in>
2017-01-02 14:11:00 +0400

    Assign a uid to each execution for better understanding of messages

--- pushwhendone.py
+++ pushwhendone.py
@@ -1,5 +1,7 @@
 import os
 import traceback
+import uuid
+
 from datetime import datetime
 from contextlib import contextmanager
 
@@ -8,6 +10,8 @@ import requests
 
 @contextmanager
 def pushwhendone(title='process', token=None):
+    title = '%s (%s)' % (title, str(uuid.uuid4())[:8])
+
     if not token:
         try:
             token = os.environ['PUSHBULLET_ACCESS_TOKEN']
@@ -21,8 +25,8 @@ def pushwhendone(title='process', token=None):
     start_time = datetime.now()
 
     start_push = {
-        'body': 'Started %s at %s' % (title, start_time),
-        'title': 'Started %s at %s' % (title, start_time),
+        'body': 'Started at %s' % (start_time),
+        'title': '[%s] Started at %s' % (title, start_time),
         'type': 'note'
     }
     response = requests.post(
@@ -39,7 +43,7 @@ def pushwhendone(title='process', token=None):
 
         end_push = {
             'body': traceback.format_exc(),
-            'title': 'Error in %s, stopped at %s' % (title, end_time),
+            'title': '[%s] Error, stopped at %s' % (title, end_time),
             'type': 'note'
         }
 
@@ -54,8 +58,8 @@ def pushwhendone(title='process', token=None):
         end_time = datetime.now()
 
         end_push = {
-            'body': 'Finished %s at %s\nStart Time: %s\nTime Taken: %s' % (title, end_time, start_time, (end_time-start_time)),
-            'title': 'Finished %s at %s' % (title, end_time),
+            'body': 'Finished %s\nStart Time: %s\nTime Taken: %s' % (end_time, start_time, (end_time-start_time)),
+            'title': '[%s] Finished at %s' % (title, end_time),
             'type': 'note'
         }
 
